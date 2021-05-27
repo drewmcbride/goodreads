03925ceac55d183e61b7085ffb3a2ddce8939d82
DrewMcB <drew.mcbride89@gmail.com>
2021-05-26 19:55:48 -0500

    Model up and running. Added app.py file.

---
+++ app.py
@@ -0,0 +1,18 @@
+"""The App."""
+
+from flask import Flask, request, jsonify
+from joblib import load
+
+clf = load("app/clf.joblib")
+
+app = Flask(__name__)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    """Takes a POST request with a key of \"text\" and the text to be classified."""
+    data_dict = request.get_json()
+
+    text = [data_dict["text"]]
+
+    return jsonify({"result": clf.predict(text)[0]})
