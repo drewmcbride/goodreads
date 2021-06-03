27766c6bf6e311d34481b5c724011a7d544da3c7
DrewMcB <drew.mcbride89@gmail.com>
2021-06-02 19:44:40 -0500

    Added correct file path and configured prediction model to app.py

--- app.py
+++ app.py
@@ -3,16 +3,24 @@
 from flask import Flask, request, jsonify
 from joblib import load
 
-clf = load("app/clf.joblib")
+clf = load("clf.joblib")
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return '<a href = "./predict?text=exampletext"> ./predict?text=exampletext</a>'
 
-@app.route("/predict", methods=["POST"])
+@app.route("/predict")
 def predict():
     """Takes a POST request with a key of \"text\" and the text to be classified."""
-    data_dict = request.get_json()
+    text = request.args.get("text")
+    # data_dict = request.get_json()
 
-    text = [data_dict["text"]]
+    # text = [data_dict["text"]]
+    print(text)
 
-    return jsonify({"result": clf.predict(text)[0]})
+    return jsonify({"result": clf.predict([text])[0]})
+
+if __name__ == "__main__":
+    app.run(debug=True)
